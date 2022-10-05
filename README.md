a0e057587ed4c2cd3a0491898f8897dbf3246bb7
oktayparlak <oktay.parlak.12@hotmail.com>
2022-10-05 20:20:03 +0300

    server created

---
+++ README.md
@@ -0,0 +1,1 @@
+#pcat demo project
--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo description',
+  };
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
