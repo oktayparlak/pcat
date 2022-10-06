a4e90005140ccf3c11a1357ca5bc5307fabdba79
oktayparlak <oktay.parlak.12@hotmail.com>
2022-10-06 16:56:55 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const port = 3000;
+//MIDDLEWARES
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
