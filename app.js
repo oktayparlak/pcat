42fcca824218d30d477da4a3904d2babfe7ba4d5
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-10-15 22:05:32 +0300

    .gitignore changed

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/photos', async (req, res) => {
   res.redirect('/');
 });
 
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
