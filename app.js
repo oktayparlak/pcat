7a1c08fb2db90c1acf8026cb6468ae06ee806474
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-10-25 17:30:15 +0300

    just commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const pageController = require('./controllers/pageController');
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 //connect DB
 mongoose
   .connect(
@@ -48,7 +50,6 @@ app.get('/about', pageController.getAboutPage);
 app.get('/add', pageController.getAddPage);
 app.get('/photos/edit/:id', pageController.getEditPage);
 
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
