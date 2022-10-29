0bc0dcb3d0981faab6a8d590ee576b22e9303782
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-10-30 00:54:51 +0300

    .

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,6 +1,8 @@
 const Photo = require('../models/Photo');
 const fs = require('fs');
-//const fileUpload = require('express-fileUpload');
+const fileUpload = require('express-fileUpload');
+
+app.use(fileUpload());
 
 exports.getAllPhotos = async (req, res) => {
   const page = req.query.page || 1;
